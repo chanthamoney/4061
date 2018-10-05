0ba5b30af61b259e511a72418acff5405883f6af
Ounngy Ing <ingxx006@umn.edu>
2018-10-05 17:03:16 -0500

    edit comment on readme and main.c

--- Project1/main.c
+++ Project1/main.c
@@ -1,8 +1,8 @@
 /* CSci4061 F2018 Assignment 1
 * login: cselabs login name (login used to submit)
-* date: mm/dd/yy
-* name: full name1, full name2, full name3 (for partner(s))
-* id: id for first name, id for second name, id for third name */
+* date: 10/05/2018
+* name: Cassandra Chanthamontry, Joey Vang, Ounngy Ing
+* id: id for first name, id for second name, ingxx006 */
 
 // This is the main file for the code
 #include <errno.h>
